901ef23b150ad41c3379cda7eed92ff2663adc5b
iZeroKim <kimgichuru6@gmail.com>
2022-09-29 17:54:09 +0300

    Create simple web server and initial  home page

--- roadmap.go
+++ roadmap.go
@@ -1,9 +1,15 @@
-package roadmap
+package main
 
 import(
 	"fmt"
+	"net/http"
 )
 
 func main(){
 	fmt.Println("Roadmap Project")
+
+	fmt.Println("Server starting")
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	fmt.Println("Server running at port http://127.0.0.1:3000")
+	http.ListenAndServe(":3000", nil)
 }
